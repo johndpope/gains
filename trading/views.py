5fb6c40e7257c5a567654fb377a8658a6983d61f
Joseph Kahihia <info@chrystalsolution.com>
2018-01-25 23:44:10 +0300

    quinine

--- trading/views.py
+++ trading/views.py
@@ -44,6 +44,8 @@ def dashboard(request , id):
 
         Quadrigacx_client = Quoinex(Quadrigacx_API.api_key, Quadrigacx_API.secret)
         Quoine_client = Quoinex(Quoinex_API.api, Quoinex_API.secret)
+        print Quadrigacx_client
+        print Quoine_client
 
 
 
