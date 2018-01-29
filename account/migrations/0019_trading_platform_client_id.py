6425f4881ae997c47b315c278e119355f6ebafac
Joseph Kahihia <info@chrystalsolution.com>
2018-01-29 16:12:44 +0300

    pyxi

--- account/migrations/0019_trading_platform_client_id.py
+++ account/migrations/0019_trading_platform_client_id.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='trading_platform',
             name='client_id',
-            field=models.CharField(default=23232, max_length=100),
-            preserve_default=False,
+            field=models.CharField(default='23232', max_length=100)
+
         ),
     ]
