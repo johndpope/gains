f09a1267cc09dc7bb5115ebb4ebeb09b30927001
Joseph Kahihia <info@chrystalsolution.com>
2018-02-01 14:02:24 +0300

    tasks

--- bluerun/celery.py
+++ bluerun/celery.py
@@ -23,7 +23,7 @@ app.config_from_object('django.conf:settings')
     #},
 #}
 # Load task modules from all registered Django app configs.
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.autodiscover_tasks()
 
 
 @app.task(bind=True)
