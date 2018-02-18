7960b6fd98d86372079aef04645ea174fbd13239
Joseph Kahihia <info@chrystalsolution.com>
2018-02-18 16:58:34 +0300

    poloniex

--- trading/exchanges/__init__.py
+++ trading/exchanges/__init__.py
@@ -2,5 +2,5 @@ from .bittrex import Bittrex
 #from .coinbase import Coinbase
 from .gdax import GDAXPrivate, GDAXPublic
 #from .kraken import Kraken
-from .poloniex import Poloniex
+from .polon import Poloniex
 #from .utils import Transaction
