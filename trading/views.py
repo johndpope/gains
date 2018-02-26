248dbf7685016568767c64d80c965a853d79a52b
Joseph Kahihia <info@chrystalsolution.com>
2018-02-26 10:50:56 +0300

    quad

--- trading/views.py
+++ trading/views.py
@@ -58,7 +58,13 @@ def dashboard(request , id):
                         }
                 data = urllib.parse.urlencode(values)
                 url = 'https://api.quadrigacx.com/v2/user_transactions?'+data
-                req = http.request('POST', url)
+                head = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+                       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+                       'Accept-Encoding': 'none',
+                       'Accept-Language': 'en-US,en;q=0.8',
+                       'Connection': 'keep-alive'}
+                req = http.request('POST', url, headers=head)
                 #response = urllib3.urlopen(req)
                 print( req.data)
                 context['Quadrigacx_data'] = ccxt.quadrigacx({
