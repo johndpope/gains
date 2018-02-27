9d2495562a678dac8c3aa0841c938e96e866e90b
Joseph Kahihia <info@chrystalsolution.com>
2018-02-27 12:28:49 +0300

    quo

--- trading/views.py
+++ trading/views.py
@@ -94,7 +94,7 @@ def dashboard(request , id):
             elif exchange == "Bitfinex" and api_credentials!=404:
                 from bitfinex.client import Client
                 client = Client("key"=api_credentials.api_key, "secret"= api_credentials.secret)\
-                print (client.past_trade()
+                print (client.past_trade())
                 context['Bitfinex_data'] = ccxt.bitfinex({"apiKey": api_credentials.api_key,
                 "secret": api_credentials.secret})
                 context['Bitfinex_transactions'], context['Bitfinex_data'] = context['Bitfinex_data'], dir(context['Bitfinex_data']) #.privatePostMytrades()
